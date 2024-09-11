9fa1b4e147255cfa82a88c869bc0de37efa3ebc7
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-11 18:41:34 +0600

    Footer is completed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,35 @@
+import { FaFacebook, FaTwitter } from 'react-icons/fa';
 import './Footer.css';
+import { FaLinkedin, FaLocationPin } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
+        <footer className="footer flex justify-between bg-orange-500 text-white p-10 shadow-md">
+            <div>
+                <h6 className="font-extrabold text-lg">Social</h6>
+                <div className='flex gap-3'>
+                <FaFacebook></FaFacebook>
+                <FaTwitter></FaTwitter>
+                <FaLinkedin></FaLinkedin>
+                </div>
+            </div>
+            <div>
+                <h6 className="font-extrabold text-lg">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Career</a>
+                <div className='flex items-center gap-2'>
+                    <FaLocationPin></FaLocationPin>
+                    <p>6/2 Kazi Nazrul Islam Rd, Dhaka 1207</p>
+                </div>
+            </div>
+            <div>
+                <h6 className="font-extrabold text-lg">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
