0b2887d8923bcd45b3c3ee3b2b3d6596ea008825
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-24 15:21:14 +0600

    Hero section are fully Responsive Now

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
     return (
         <div className='hero-section flex justify-between items-center m-10 border-2 border-orange-600'>
             <div className="hero-left flex flex-col items-center w-1/2 ">
-                <p className='text-left'>OUR BEST SELL PRODUCT</p>
-                <p className='text-left'>Latest Arrival</p>
-                <p className='text-left'>SHOP NOW</p>
+                <p className='text-sm'>OUR BEST SELL PRODUCT</p>
+                <p className='text-3xl font-bold'>Latest Arrival</p>
+                <p className='text-sm bg-orange-500 p-2 rounded-md'>SHOP NOW</p>
             </div>
-            <div className=' w-1/2 h-[300px]'>
+            <div className='hero-right w-1/2 h-[400px]'>
                 <img src={img} alt="" className='w-full h-full' />
             </div>
         </div>
