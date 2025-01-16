ebd6b442d7a35ca6b976c23d3240f331e98e1d92
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-01-16 17:05:08 +0600

    Porduct list API updated

--- frontend/src/pages/Product/Product.jsx
+++ frontend/src/pages/Product/Product.jsx
@@ -31,7 +31,7 @@ const Product = () => {
             <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
                 {/* product Images  */}
                 <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
-                    <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+                    <div className='flex sm:flex-col overflow-x-auto justify-between sm:justify-normal sm:w-[18.7%] w-full'>
                             {
                                 productData?.images?.map((item, index)=><img onClick={()=>setImages(item)}
                                 src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'
